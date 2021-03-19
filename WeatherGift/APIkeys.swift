075e09dfd2267a5a26b06d8c8280c80936c5245a
Chelsea Schwartz <chelseaschwartz3@gmail.com>
2021-03-19 14:24:35 -0400

    Test

---
+++ WeatherGift/APIkeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIkeys.swift
+//  WeatherGift
+//
+//  Created by Chelsea Schwartz on 3/16/21.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyAew4_rCoOSmhMUxl-Z-vnj0UKiEzAyRv8"
+}
+
